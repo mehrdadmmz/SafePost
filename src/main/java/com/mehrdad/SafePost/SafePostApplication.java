c59a45f742ba23ab8106a52c0f7d696b89d95803
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2025-09-04 22:45:07 -0700

    Starter code of the SafePost project

---
+++ src/main/java/com/mehrdad/SafePost/SafePostApplication.java
@@ -0,0 +1,13 @@
+package com.mehrdad.SafePost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehrdad/SafePost/SafePostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehrdad.SafePost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
