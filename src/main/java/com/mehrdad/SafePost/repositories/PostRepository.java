e88bdb340c4b046d972b945dce72eef4e37c2986
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2025-09-29 23:09:21 -0700

    getting all post through the service layer

--- src/main/java/com/mehrdad/SafePost/repositories/PostRepository.java
+++ src/main/java/com/mehrdad/SafePost/repositories/PostRepository.java
@@ -1,11 +1,18 @@
 package com.mehrdad.SafePost.repositories;
 
+import com.mehrdad.SafePost.domain.PostStatus;
+import com.mehrdad.SafePost.domain.entities.Category;
 import com.mehrdad.SafePost.domain.entities.Post;
+import com.mehrdad.SafePost.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    // we need the post status because we only need the published posts
+    // the name of the function is by convention so that the spring data JpaRepository can implement it for us.
+    List<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tag);
 }
--- src/main/java/com/mehrdad/SafePost/services/impl/PostServiceImpl.java
+++ src/main/java/com/mehrdad/SafePost/services/impl/PostServiceImpl.java
@@ -1,13 +1,16 @@
 package com.mehrdad.SafePost.services.impl;
 
+import com.mehrdad.SafePost.domain.PostStatus;
 import com.mehrdad.SafePost.domain.entities.Category;
 import com.mehrdad.SafePost.domain.entities.Post;
+import com.mehrdad.SafePost.domain.entities.Tag;
 import com.mehrdad.SafePost.repositories.PostRepository;
 import com.mehrdad.SafePost.services.CategoryService;
 import com.mehrdad.SafePost.services.PostService;
 import com.mehrdad.SafePost.services.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,8 +24,18 @@ public class PostServiceImpl implements PostService {
     private final TagService tagService;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Post> getAllPosts(UUID categoryId, UUID tagId) {
-        Category category = categoryService.getCategoryById(categoryId);
-        Tag tag = tagService.
+
+        // when both category id and tag id are specified
+        if (categoryId != null && tagId != null) {
+            Category category = categoryService.getCategoryById(categoryId);
+            Tag tag = tagService.getTagByID(tagId);
+            return postRepository.findAllByStatusAndCategoryAndTagsContaining(
+                    PostStatus.PUBLISHED,
+                    category,
+                    tag
+            );
+        }
     }
 }
